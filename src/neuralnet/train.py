4628d8a9cc29867a7996ddaada82daf2bbef187d
Oleksandr Maistrenko <al.master2000@gmail.com>
2025-05-04 21:52:35 +0300

    Move to pytorch-ignite

--- src/neuralnet/train.py
+++ src/neuralnet/train.py
@@ -1,24 +1,44 @@
 import torch
-from sklearn.metrics import roc_auc_score
+from ignite.engine import Engine, Events
+from ignite.metrics import ROC_AUC, Loss
+from ignite.handlers import ModelCheckpoint
+from ignite.contrib.handlers import global_step_from_engine, TensorboardLogger
+from ignite.handlers.early_stopping import EarlyStopping
 import matplotlib.pyplot as plt
 
 
 def train_model(model, train_loader, val_loader, columns_idx, criterion, optimizer, epochs=10):
-    # Empty lists to track metrics
-    epoch_count, train_loss_values, valid_loss_values, valid_metric_values = [], [], [], []
-
-    # Early stopping parameters
-    patience = int(epochs)
-    best_val_loss = float("inf")
-    best_epoch = -1
-    early_stop_counter = 0
-
-    for epoch in range(epochs):
+    def train_step(engine, batch):
         model.train()
-        model.sentiment_model.train()
-        running_loss = 0.0  # Initialize running loss for each epoch
 
-        for i, (inputs, labels) in enumerate(train_loader):
+        # Zero the parameter gradients
+        optimizer.zero_grad()
+
+        inputs, labels = batch[0], batch[1]
+        input_ids = inputs[:, columns_idx["INPUT_IDS_START"] : columns_idx["INPUT_IDS_END"]]
+        attention_mask = inputs[
+            :, columns_idx["ATTENTION_MASK_START"] : columns_idx["ATTENTION_MASK_END"]
+        ]
+        x_cat = inputs[:, columns_idx["CATEGORICAL_START"] : columns_idx["CATEGORICAL_END"]]
+        x_cont = inputs[:, columns_idx["NUMERIC_START"] : columns_idx["NUMERIC_END"]]
+
+        # Forward pass
+        outputs = model(input_ids, attention_mask, x_cat, x_cont).squeeze()
+        loss = criterion(outputs, labels)
+
+        # Backward pass and optimize
+        loss.backward()
+        optimizer.step()
+        return loss.item()
+
+    trainer = Engine(train_step)
+
+    val_metrics = {"roc_auc_score": ROC_AUC(), "loss": Loss(criterion)}
+
+    def validation_step(engine, batch):
+        model.eval()
+        with torch.no_grad():
+            inputs, labels = batch[0], batch[1]
             input_ids = inputs[:, columns_idx["INPUT_IDS_START"] : columns_idx["INPUT_IDS_END"]]
             attention_mask = inputs[
                 :, columns_idx["ATTENTION_MASK_START"] : columns_idx["ATTENTION_MASK_END"]
@@ -26,98 +46,82 @@ def train_model(model, train_loader, val_loader, columns_idx, criterion, optimiz
             x_cat = inputs[:, columns_idx["CATEGORICAL_START"] : columns_idx["CATEGORICAL_END"]]
             x_cont = inputs[:, columns_idx["NUMERIC_START"] : columns_idx["NUMERIC_END"]]
 
-            # Zero the parameter gradients
-            optimizer.zero_grad()
-
             # Forward pass
             outputs = model(input_ids, attention_mask, x_cat, x_cont).squeeze()
-            loss = criterion(outputs, labels)
+            return outputs, labels
 
-            # Backward pass and optimize
-            loss.backward()
-            optimizer.step()
+    train_evaluator = Engine(validation_step)
+    val_evaluator = Engine(validation_step)
 
-            # Accumulate running loss
-            running_loss += loss.item()
+    # Attach metrics to the evaluators
+    for name, metric in val_metrics.items():
+        metric.attach(train_evaluator, name)
 
-        # Validation phase
-        model.eval()
-        model.sentiment_model.eval()
-        val_loss = 0.0
-        all_labels, all_preds = [], []  # To store labels and predictions for ROC-AUC
+    for name, metric in val_metrics.items():
+        metric.attach(val_evaluator, name)
 
-        with torch.no_grad():
-            for inputs, labels in val_loader:
-                input_ids = inputs[
-                    :, columns_idx["INPUT_IDS_START"] : columns_idx["INPUT_IDS_END"]
-                ]
-                attention_mask = inputs[
-                    :, columns_idx["ATTENTION_MASK_START"] : columns_idx["ATTENTION_MASK_END"]
-                ]
-                x_cat = inputs[
-                    :, columns_idx["CATEGORICAL_START"] : columns_idx["CATEGORICAL_END"]
-                ]
-                x_cont = inputs[:, columns_idx["NUMERIC_START"] : columns_idx["NUMERIC_END"]]
+    log_interval = 100
 
-                outputs = model(input_ids, attention_mask, x_cat, x_cont).squeeze()
-                loss = criterion(outputs, labels)
-                val_loss += loss.item()
+    @trainer.on(Events.ITERATION_COMPLETED(every=log_interval))
+    def log_training_loss(engine):
+        print(
+            f"Epoch[{engine.state.epoch}], Iter[{engine.state.iteration}] Loss: {engine.state.output:.2f}"
+        )
 
-                # Store labels and outputs for ROC-AUC calculation
-                all_labels.extend(labels.cpu().numpy())
-                all_preds.extend(outputs.cpu().numpy())
+    @trainer.on(Events.EPOCH_COMPLETED(every=epochs // 25))
+    def log_training_results(trainer):
+        train_evaluator.run(train_loader)
+        metrics = train_evaluator.state.metrics
+        print(
+            f"Training Results - Epoch[{trainer.state.epoch}] Avg roc_auc_score: {metrics['roc_auc_score']:.2f} Avg loss: {metrics['loss']:.2f}"
+        )
 
-        # Average the losses over the dataset sizes
-        avg_train_loss = running_loss / len(train_loader)
-        avg_val_loss = val_loss / len(val_loader)
+    @trainer.on(Events.EPOCH_COMPLETED(every=epochs // 25))
+    def log_validation_results(trainer):
+        val_evaluator.run(val_loader)
+        metrics = val_evaluator.state.metrics
+        print(
+            f"Validation Results - Epoch[{trainer.state.epoch}] Avg roc_auc_score: {metrics['roc_auc_score']:.2f} Avg loss: {metrics['loss']:.2f}"
+        )
 
-        # Calculate ROC-AUC for the validation set
-        val_metric = roc_auc_score(all_labels, all_preds)
+    def score_function(engine):
+        return engine.state.metrics["roc_auc_score"]
 
-        # Early stopping logic
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
-            best_epoch = epoch + 1
-            early_stop_counter = 0  # Reset counter if validation loss improves
-        else:
-            early_stop_counter += 1  # Increment counter if no improvement
+    model_checkpoint = ModelCheckpoint(
+        "checkpoint",
+        n_saved=1,
+        filename_prefix="best",
+        score_function=score_function,
+        score_name="roc_auc_score",
+        global_step_transform=global_step_from_engine(trainer),
+    )
 
-        if early_stop_counter >= patience:
-            print("Early stopping triggered!")
-            print(
-                f"Epoch [{epoch+1}/{epochs}], Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Val ROC-AUC: {val_metric:.4f}"
-            )
-            break
+    val_evaluator.add_event_handler(Events.COMPLETED, model_checkpoint, {"model": model})
 
-        # Store epoch metrics
-        if (epochs - epoch - 1) % int(epochs / 25) == 0:
-            print(
-                f"Epoch [{epoch+1}/{epochs}], Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Val ROC-AUC: {val_metric:.4f}"
-            )
-            epoch_count.append(epoch)
-            train_loss_values.append(avg_train_loss)
-            valid_loss_values.append(avg_val_loss)
-            valid_metric_values.append(val_metric)
+    early_stopping = EarlyStopping(
+        patience=10, score_function=lambda engine: -engine.state.metrics["loss"], trainer=trainer
+    )
+    # Note: the handler is attached to an *Evaluator* (runs one epoch on validation dataset).
+    val_evaluator.add_event_handler(Events.COMPLETED, early_stopping)
 
-    print("BEST EPOCH:", best_epoch)
-    print("BEST LOSS:", best_val_loss)
+    tb_logger = TensorboardLogger(log_dir="tb-logger")
 
-    # Plot Loss Curves
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(epoch_count, train_loss_values, label="Training Loss")
-    plt.plot(epoch_count, valid_loss_values, label="Validation Loss")
-    plt.title("Training & Validation Loss Curves")
-    plt.ylabel("Loss")
-    plt.xlabel("Epochs")
-    plt.legend()
+    tb_logger.attach_output_handler(
+        trainer,
+        event_name=Events.ITERATION_COMPLETED(every=log_interval),
+        tag="training",
+        output_transform=lambda loss: {"batch_loss": loss},
+    )
 
-    # Plot ROC-AUC Curve
-    plt.subplot(1, 2, 2)
-    plt.plot(epoch_count, valid_metric_values, label="Validation ROC-AUC")
-    plt.title("Validation ROC-AUC Curve")
-    plt.ylabel("ROC-AUC")
-    plt.xlabel("Epochs")
-    plt.legend()
+    for tag, evaluator in [("training", train_evaluator), ("validation", val_evaluator)]:
+        tb_logger.attach_output_handler(
+            evaluator,
+            event_name=Events.EPOCH_COMPLETED,
+            tag=tag,
+            metric_names=["roc_auc_score", "loss"],
+            global_step_transform=global_step_from_engine(trainer),
+        )
 
-    plt.show()
+    trainer.run(train_loader, max_epochs=epochs)
+
+    tb_logger.close()
